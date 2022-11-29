419919327d24e0e96a35998eb80b585e245cf771
nvrxq <kutuzinstaccount@mail.ru>
2022-11-30 01:53:39 +0300

    Added tests

--- 01/tests.cpp
+++ 01/tests.cpp
@@ -10,32 +10,44 @@ TEST(test_allocator, alloc)
 {
     Allocator al;
     al.makeAllocator(10);
+    // Add memory
     {
         char *res = al.alloc( 5 );
         ASSERT_TRUE( res != nullptr );
     }
 
-   
+    //Overflow
     {
         char *res = al.alloc(6);
         ASSERT_EQ(res, nullptr);
     }
-
+    // Add Memory
     {
         char *res = al.alloc(5);
         ASSERT_TRUE(res != nullptr);
-    }
-
+    } 
+    // Reset
     al.reset();
-    al.makeAllocator(2);
+    al.makeAllocator(10);
     {
-        char *res = al.alloc( 1 );
+        char *res = al.alloc(2);
         ASSERT_TRUE( res != nullptr );
     }
+    
+    {
+	    char *res = al.alloc(9);
+	    ASSERT_EQ(res, nullptr);
+    }
+    al.reset();
 
     {
+	    char *res = al.alloc(9);
+	    ASSERT_TRUE(res != nullptr);
+    }
+    // Chech getter
+    {
 	size_t offset = al.getOffset();
-	ASSERT_EQ(offset,1);
+	ASSERT_EQ(offset,9);
     }
 }
 
