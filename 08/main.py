ce7dac43c3216119d9ca3def6b7906aeeb51f88f
nvrxq <kutuzinstaccount@mail.ru>
2023-03-27 22:16:05 +0300

    hw8 at1

--- 08/main.py
+++ 08/main.py
@@ -28,6 +28,28 @@ def test_load():
     assert js_time > cj_time # В 1/10 не пройдет.
 
 
+def test_load_digit():
+    faker = Faker()
+    json_test = {}
+    for _ in range(1000):
+        json_test[faker.first_name()] = randint(1, 10)
+    json_test = ujson.dumps(json_test)
+    #---------TEST_VALUE AND TIME--------#
+    uj = ujson.loads(json_test)
+    #---------------------------------------#
+    start=datetime.now()
+    jj = json.loads(json_test)
+    js_time = datetime.now()-start
+    #---------------------------------------#
+    start=datetime.now()
+    cj = cjson.load(json_test)
+    cj_time = datetime.now()-start
+
+    
+    assert uj == jj == cj
+    assert js_time > cj_time # В 1/10 не пройдет.
+
+
 
 def test_dumps():
     faker = Faker()
